eb8e3f1251f2fd5b2946c1b6d91d8f758370ab87
dkovacevic <dejankov@gmail.com>
2020-10-26 12:36:57 +0100

    JDBIv3

--- src/main/java/com/wire/xenon/crypto/storage/IdentitiesDAO.java
+++ src/main/java/com/wire/xenon/crypto/storage/IdentitiesDAO.java
@@ -1,11 +1,18 @@
 package com.wire.xenon.crypto.storage;
 
-import org.skife.jdbi.v2.sqlobject.Bind;
-import org.skife.jdbi.v2.sqlobject.SqlQuery;
-import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.jdbi.v3.core.mapper.ColumnMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public interface IdentitiesDAO {
-    @SqlUpdate("INSERT INTO Identities (id, data) VALUES (:id, :data) ON CONFLICT (id) DO UPDATE SET data = EXCLUDED.data")
+    @SqlUpdate("INSERT INTO Identities (id, data) " +
+            "VALUES (:id, :data) " +
+            "ON CONFLICT (id) DO UPDATE SET data = EXCLUDED.data")
     int insert(@Bind("id") String id,
                @Bind("data") byte[] data);
 
@@ -14,4 +21,11 @@ public interface IdentitiesDAO {
 
     @SqlUpdate("DELETE FROM Identities WHERE id = :id")
     int delete(@Bind("id") String id);
+
+    class _Mapper implements ColumnMapper<byte[]> {
+        @Override
+        public byte[] map(ResultSet r, int columnNumber, StatementContext ctx) throws SQLException {
+            return r.getBytes("data");
+        }
+    }
 }
